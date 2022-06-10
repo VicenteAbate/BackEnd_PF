2be127d4b5f8a36eb55d2304b0c5041d9aa4268a
natmacira <natimciraolo@gmail.com>
2022-06-10 09:20:15 -0300

    agregado el post

--- Back para Pokedex/Routes/ruta.js
+++ Back para Pokedex/Routes/ruta.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router();
 const axios = require("axios");
-const { getPokemonsApi, getUsers, login, createPokemon, getPokeObjetTest } = require("../controller/control")
+const { getPokemonsApi, getUsers, login, createPokemon, getPokeObjetTest, createPokemonForApi } = require("../controller/control");
 
 
-router.get("/", getPokemonsApi)
+router.get("/", getPokemonsApi);
+router.post("/", createPokemonForApi);
 
 
 router.get("/ArrayPokemons", getPokeObjetTest);
 router.post("/ArrayPokemons", createPokemon)
 
 
-router.get("/usuarios", getUsers)
-router.post("/usuarios/login", login)
+router.get("/usuarios", getUsers);
+router.post("/usuarios/login", login);
 
 
 module.exports = router;
